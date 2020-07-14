cf0ed30a56e5d416c8d7ebdab7da8afdb1ba0510
Amit Verma <amitverma.ukind@gmail.com>
2020-07-14 18:29:08 +0530

    Tween to separated Meshes

--- main.js
+++ main.js
@@ -1,10 +1,6 @@
 import CSG from "./lib/CSGMesh.js";
 var scene, camera, renderer, controls;
 
-var mesh3bool = false;
-var mesh4bool = false;
-var mesh1, mesh2, mesh3, mesh4, mesh5;
-
 init();
 
 function init() {
@@ -41,8 +37,6 @@ function init() {
   ground.position.y = -100;
   scene.add(ground);
 
-  deformDemo();
-
   animate();
   window.scene = scene;
 }
@@ -52,8 +46,7 @@ function animate() {
 
   renderer.render(scene, camera);
   controls.update();
-  if (mesh3bool) mesh3.position.y += 0.1;
-  if (mesh4bool) mesh4.position.y -= 0.1;
+  TWEEN.update();
 }
 
 function createLight() {
@@ -79,41 +72,7 @@ function createLight() {
 
   scene.add(dirLight);
 }
-function deformDemo() {
-  mesh1 = new THREE.Mesh(
-    new THREE.BoxGeometry(50, 50, 50),
-    new THREE.MeshPhongMaterial({
-      color: 0xff0000,
-      shininess: 100,
-    })
-  );
-  mesh1.position.x = 0;
-  
-  scene.add(mesh1);
 
-  mesh2 = new THREE.Mesh(
-    new THREE.BoxGeometry(50, 50, 50),
-    new THREE.MeshPhongMaterial({
-      color: 0x00ff00,
-      shininess: 100,
-    })
-  );
-  mesh2.position.x = -25;
-  mesh3 = new THREE.Mesh(
-    new THREE.BoxGeometry(50, 50, 50),
-    new THREE.MeshPhongMaterial({
-      color: 0x0000ff,
-      shininess: 100,
-    })
-  );
-  mesh3.position.x = 25;
-  mesh2.visible = false;
-  mesh3.visible = false;
-  scene.add(mesh2);
-  scene.add(mesh3);
-  mesh1.updateMatrix();
-  mesh2.updateMatrix();
-}
 
 function doCSG(a, b, op, mat) {
   var bspA = CSG.fromMesh(a);
@@ -125,30 +84,140 @@ function doCSG(a, b, op, mat) {
   return result;
 }
 
-function doOperation() {
+
+var cubeMeshes = cubeDemo();
+var sphereMeshes = sphereDemo();
+
+window.addEventListener("keypress", event => {
+  if ( event.key === 'c') 
+    {
+        
+        
+        doOperation(cubeMeshes[0], cubeMeshes[1], cubeMeshes[2]);
+        
+        
+    }
+    if ( event.key === 's') 
+    {
+        
+        
+        doOperation(sphereMeshes[0], sphereMeshes[1], sphereMeshes[2], true);
+        
+        
+    }
+  });
+
+
+function cubeDemo()
+{
+  var mesh1 = new THREE.Mesh(
+    new THREE.BoxGeometry(50, 50, 50),
+    new THREE.MeshPhongMaterial({
+      color: 0xff0000,
+      shininess: 100,
+    })
+  );
+  mesh1.position.x = 0;
+  
+  
+
+  var mesh2 = new THREE.Mesh(
+    new THREE.BoxGeometry(50, 50, 50),
+    new THREE.MeshPhongMaterial({
+      color: 0x00ff00,
+      shininess: 100,
+    })
+  );
+  mesh2.position.x = -25;
+  var mesh3 = new THREE.Mesh(
+    new THREE.BoxGeometry(50, 50, 50),
+    new THREE.MeshPhongMaterial({
+      color: 0x0000ff,
+      shininess: 100,
+    })
+  );
+  mesh3.position.x = 25;
+  mesh2.visible = false;
+  mesh3.visible = false;
+  scene.add(mesh1);
+  scene.add(mesh2);
+  scene.add(mesh3);
+  mesh1.updateMatrix();
+  mesh2.updateMatrix();
+  mesh3.updateMatrix();
+  return [mesh1, mesh2, mesh3];
+}
+
+
+function sphereDemo()
+{
+  var mesh1 = new THREE.Mesh(
+    new THREE.SphereGeometry(25, 50, 50),
+    new THREE.MeshPhongMaterial({
+      color: 0xff0000,
+      shininess: 100,
+    })
+  );
+  mesh1.position.x = 0;
+  
+  
+
+  var mesh2 = new THREE.Mesh(
+    new THREE.BoxGeometry(50, 50, 50),
+    new THREE.MeshPhongMaterial({
+      color: 0x00ff00,
+      shininess: 100,
+    })
+  );
+  mesh2.position.x = -25;
+  var mesh3 = new THREE.Mesh(
+    new THREE.BoxGeometry(50, 50, 50),
+    new THREE.MeshPhongMaterial({
+      color: 0x0000ff,
+      shininess: 100,
+    })
+  );
+  mesh3.position.x = 25;
+  mesh2.visible = false;
+  mesh3.visible = false;
+  scene.add(mesh1);
+  scene.add(mesh2);
+  scene.add(mesh3);
+  mesh1.updateMatrix();
+  mesh2.updateMatrix();
+  mesh3.updateMatrix();
+  mesh1.position.z = 200;
+  mesh2.position.z = 200;
+  mesh3.position.z = 200;
+  return [mesh1, mesh2, mesh3];
+}
+
+
+function doOperation(mesh1, mesh2, mesh3, wireFrame) {
   mesh1.visible = false;
-  mesh3 = doCSG(
+  var mesh4 = doCSG(
     mesh1,
     mesh2,
     "subtract",
     new THREE.MeshPhongMaterial({
-      color: 0x00ff00,
+      color: 0xff0000,
       shininess: 100,
+      wireframe:wireFrame
     })
   );
-  scene.add(mesh3);
-  mesh3bool = true;
-  mesh4 = doCSG(
+  scene.add(mesh4);
+  var mesh5 = doCSG(
     mesh1,
     mesh3,
     "subtract",
     new THREE.MeshPhongMaterial({
-      color: 0x00ff00,
+      color: 0xff0000,
       shininess: 100,
+      wireframe:wireFrame
     })
   );
-  scene.add(mesh4);
-  mesh4bool = true;
+  
+  scene.add(mesh5);
+  var tween = new TWEEN.Tween(mesh4.position).to({ y:100, x:100 }, 10000).start();
+  var tween2 = new TWEEN.Tween(mesh5.position).to({ y:-100,x:-100 }, 10000).start();
 }
-
-window.addEventListener("click", doOperation);
